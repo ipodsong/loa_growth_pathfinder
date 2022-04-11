43f9f26d70e01b7c1d0784fe85788586bc9ff9b2
nonu1017 <55435652+nonu1017@users.noreply.github.com>
2022-04-11 16:06:35 +0900

    #1 버튼만들기

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from './components/Button';
+import './App.css';
+
+
+function App() {
+    return ( <
+        div className = 'App' >
+        <
+        div className = 'buttons' >
+        <
+        Button > BUTTON < /Button> < /
+        div > <
+        /div>
+    );
+}
+
+
+export default App;
