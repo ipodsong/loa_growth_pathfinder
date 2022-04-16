42f995827bed7d4b1210b0bde1c4daf34a0a2d54
nonu1017 <55435652+nonu1017@users.noreply.github.com>
2022-04-16 16:46:19 +0900

    #3 수정2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {BrowserRouter as  Router , Route, Routes} from "react-router-dom";
+import Home from './pages/home.js'
+import Res from './pages/res.js'
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./pages/home.js";
-import Res from "./pages/res.js"
-import NotFound from "./pages/notFound.js"
-
-
 
 
 function App() {
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-    return ( <
-        div className = 'App' >
-        <
-        div className = 'buttons' >
-        <
-        Button > BUTTON < /Button> < /
-        div > <
-        /div>
-    );
-=======
->>>>>>> Stashed changes
+
+
     
         return(
             <div className='App'>
@@ -30,11 +15,7 @@ function App() {
             <Router>
                 <Routes>
                     <Route path='/' element={<Home />} />
-<<<<<<< Updated upstream
-                    <Route path='/res' element={<Res />} />
-=======
                     <Route path='/res/user/:userName' element={<Res />} />
->>>>>>> Stashed changes
                     <Route path='/*' element={<p>not found</p>} />
                 </Routes>
             </Router>
@@ -42,12 +23,8 @@ function App() {
             </div>
         );
     
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-}
 
+}
 
 
 export default App;
